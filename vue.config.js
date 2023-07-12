32dc473e87799783cb06d7a15363b814c24b6d65
Nizami Jussupov <nizami.jussupov@nu.edu.kz>
2023-07-12 13:00:13 +0600

    added  404.html

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/windbnb/" : "/",
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
+}
